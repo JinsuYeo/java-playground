2740207bcb7fe5b90784e8260ec430fcb5ceebe5
jsyeo97 <jsyeo97@naver.com>
2022-11-06 03:53:51 +0900

    feat : 1~9 까지의 서로 다른 임의의 3자리 수 생성 기능 구현

--- src/main/java/unit/BaseBallGame.java
+++ src/main/java/unit/BaseBallGame.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class BaseBallGame {
-	private static ArrayList<Integer> randomNumbers = new ArrayList<>(); 
-	private static ArrayList<Integer> userInputNumbers = new ArrayList<>();
+public class BaseBallGame {	
+	private static ArrayList<Integer> randomNumbers;
+	private static ArrayList<Integer> userInputNumbers;
 	
 	public static void startGame(Scanner scanner) {
 		while(true) {
@@ -14,10 +14,45 @@ public class BaseBallGame {
 			baseBallGame(scanner);
 		}
 	}
+	
+	public static boolean baseBallGame(Scanner scanner) {
+		while(true) {
+			
+		}
+	}
+	
+	private static boolean isDuplicated(ArrayList<Integer> randomNumbers, int randomNumber) {
+		if(randomNumber == 0) return true;
+		ArrayList<Integer> randomNumbersNotAddedRandomNumber = randomNumbers;
+		randomNumbers.add(randomNumber);
+		if(randomNumbersNotAddedRandomNumber.size() == randomNumbers.stream().distinct().count()) {
+			return false;
+		}
+		return true;
+	}
+	
+	private static int makeRandomNumber(ArrayList<Integer> randomNumbers) {
+		Random random = new Random();
+		int randomNumber = 0;
+		while(isDuplicated(randomNumbers, randomNumber))
+			randomNumber = 1 + random.nextInt(8);
+		return randomNumber;
+	}
+	
+	public static ArrayList<Integer> makeRandomNumbers() {
+		ArrayList<Integer> randomNumbers = new ArrayList<>();
+		for(int i = 0; i < 3; i++) {
+			randomNumbers.add(makeRandomNumber(randomNumbers));
+		}
+		
+		return randomNumbers;
+	}
 
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		randomNumbers = makeRandomNumbers();
 		startGame(scanner);
 	}
 }
+
