a1b2cceccc80cef87016282a8368710feeea4b6c
jsyeo97 <jsyeo97@naver.com>
2022-11-06 19:40:30 +0900

    feat : 스트라이크 개수, 볼 개수 판별 후 결과 출력 및 재시작/종료 입력받아 재시작/종료 기능 구현

--- src/main/java/unit/BaseBallGame.java
+++ src/main/java/unit/BaseBallGame.java
@@ -7,15 +7,48 @@ import java.util.Random;
 public class BaseBallGame {	
 	private static ArrayList<Integer> randomNumbers;
 	private static ArrayList<Integer> userInputNumbers;
+	private static int strike = 0;
+	private static int ball = 0;
 	
-	public static void startGame(Scanner scanner) {
-		while(true) {
-			System.out.println("숫자 야구 게임을 시작합니다.");
-			baseBallGame(scanner);
-			break;
-		}
+	public static void initBaseBallGame() {
+		userInputNumbers = null;
+		strike = 0;
+		ball = 0;
 	}
 	
+	public static void startGame(Scanner scanner) {
+		boolean restartGame = true;
+		while(restartGame) {
+			System.out.println("숫자 야구 게임을 시작합니다.");
+			randomNumbers = makeRandomNumbers();
+			restartGame = baseBallGame(scanner);
+		}
+	}
+
+	public static boolean baseBallGame(Scanner scanner) {
+		boolean restartGame = false;
+		while(!isThreeStrike()) {
+			System.out.print("숫자를 입력해 주세요 : ");
+			int inputNum = scanner.nextInt();
+			userInputNumbers = makeUserInputNumbers(inputNum);
+			checkIllegalInput(userInputNumbers);
+			compare();
+			printOutResult();
+			initBaseBallGame();
+		}
+		restartGame = restartOrExit(scanner);
+		return restartGame;
+	}
+
+	private static boolean restartOrExit(Scanner scanner) {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		int input = scanner.nextInt();
+		if(input == 1) return true;
+		if(input == 2) return false;
+		exit();
+		return false;
+	}
+
 	public static ArrayList<Integer> makeUserInputNumbers(int inputNum) {
 		ArrayList<Integer> userInputNumbers = new ArrayList<>();
 		int num = inputNum;
@@ -27,19 +60,27 @@ public class BaseBallGame {
 	}
 	
 	private static void compare() {
-		
+		for(int i = 0; i < 3; i++) {
+			strikeOrBall(i);
+		}
 	}
 	
-	public static boolean baseBallGame(Scanner scanner) {
-		while(true) {
-			System.out.println("숫자를 입력해 주세요 : ");
-			int inputNum = scanner.nextInt();
-			userInputNumbers = makeUserInputNumbers(inputNum);
-			checkIllegalInput(userInputNumbers);
-			compare();
-			break;
+	private static void strikeOrBall(int idx) {
+		if(!isStrike(randomNumbers.get(idx), userInputNumbers.get(idx)))
+			isBall(randomNumbers, userInputNumbers.get(idx));
+	}
+
+	
+	private static boolean isStrike(Integer randomNum, Integer userInputNum) {
+		if(randomNum == userInputNum) {
+			strike++;
+			return true;
 		}
-		return true;
+		return false;
+	}
+
+	private static void isBall(ArrayList<Integer> randomNumbers, Integer userInputNum) {
+		if(randomNumbers.contains(userInputNum)) ball++;
 	}
 
 	private static void checkIllegalInput(ArrayList<Integer> userInputNumbers) {
@@ -81,11 +122,39 @@ public class BaseBallGame {
 		
 		return randomNumbers;
 	}
-
+	
+	private static void printOutResult() {
+		if(strike == 0 && ball == 0) {
+			System.out.println("낫싱");
+			return;
+		}
+		
+		if(strike > 0 && ball > 0) {
+			System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+			return;
+		}
+		
+		if(strike > 0 && ball == 0) {
+			System.out.printf("%d스트라이크\n", strike);
+			return;
+		}
+		
+		if(ball > 0 && strike == 0) {
+			System.out.printf("%d볼\n", ball);
+			return;
+		}
+	}
+	
+	private static boolean isThreeStrike() {
+		if(strike == 3) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			return true;
+		}
+		return false;
+	}
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		randomNumbers = makeRandomNumbers();
 		startGame(scanner);
 	}
 }
