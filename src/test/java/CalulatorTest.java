573b405d0f92a90d6f0f5f03928780a91e9ea9ab
jsyeo97 <jsyeo97@naver.com>
2022-12-04 12:39:18 +0900

    test: 문자열이 구분자인지 테스트 구현

--- src/test/java/CalulatorTest.java
+++ src/test/java/CalulatorTest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CalulatorTest {
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CalulatorTest {
+    @Test
+    void 숫자_테스트() {
+        assertThat(Calculator.isInt("1")).isTrue();
+        assertThatThrownBy(() -> Calculator.isInt("a")).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 구분자_테스트() {
+        assertThat(Calculator.isDelimeter(":")).isTrue();
+    }
+
 }
