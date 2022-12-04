92ee72cb3ba2a9698c85d1a05bcb67f89fc9a24e
jsyeo97 <jsyeo97@naver.com>
2022-12-04 12:49:52 +0900

    문자열이 구분자인지 확인 기능 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Calculator {
+    private static List<String> delimeters = Arrays.asList(":", ",");
+
     public static boolean isInt(String s) throws RuntimeException {
         try {
             int i = Integer.parseInt(s);
@@ -11,4 +13,9 @@ public class Calculator {
         }
         return true;
     }
+
+    public static boolean isDelimeter(String s) {
+        return delimeters.stream().anyMatch(str -> s.equals(str));
+    }
+
 }
