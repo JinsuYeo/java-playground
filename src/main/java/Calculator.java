dd74d9b2ac8bfafa4e3ee02cf3e6a39b469d2e3a
jsyeo97 <jsyeo97@naver.com>
2022-12-05 21:57:44 +0900

    문자열 구분자로 분리 후 더하는 기능 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
-    private static List<String> delimeters = Arrays.asList(":", ",");
+    private static ArrayList<String> delimeters = new ArrayList<String>(Arrays.asList(":", ","));
 
     public static boolean isInt(String s) throws RuntimeException {
         try {
@@ -18,4 +20,41 @@ public class Calculator {
         return delimeters.stream().anyMatch(str -> s.equals(str));
     }
 
+    public static boolean isPositive(String s) {
+        try {
+            int i = Integer.parseInt(s);
+            if (i < 0) throw new Exception();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+
+    public static int splitAndSum(String s) {
+        int sum = 0;
+        String text = s;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            delimeters.add(customDelimiter);
+            text = m.group(2);
+        }
+        String del = "";
+        for (int i = 0; i < delimeters.size(); i++) {
+            del += delimeters.get(i) + "|";
+        }
+        del = del.substring(0, del.length()-1);
+        String[] nums = text.split(del);
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String in = scanner.nextLine();
+        int sum = splitAndSum(in);
+        System.out.println(sum);
+    }
 }
