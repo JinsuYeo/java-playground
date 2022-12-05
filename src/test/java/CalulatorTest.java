5d63a7d14350ef893b29d19039ce1528e30260a9
jsyeo97 <jsyeo97@naver.com>
2022-12-05 21:56:13 +0900

    test: 문자열 구분자로 분리 후 더하는 기능 테스트 구현

--- src/test/java/CalulatorTest.java
+++ src/test/java/CalulatorTest.java
@@ -8,6 +8,7 @@ public class CalulatorTest {
     void 숫자_테스트() {
         assertThat(Calculator.isInt("1")).isTrue();
         assertThatThrownBy(() -> Calculator.isInt("a")).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> Calculator.isInt(" ")).isInstanceOf(RuntimeException.class);
     }
 
     @Test
@@ -17,4 +18,17 @@ public class CalulatorTest {
         assertThat(Calculator.isDelimeter(".")).isFalse();
     }
 
+    @Test
+    void 양수_테스트() {
+        assertThat(Calculator.isPositive("1")).isTrue();
+        assertThatThrownBy(() -> Calculator.isPositive("-1")).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void splitAndSum_테스트() {
+        assertThat(Calculator.splitAndSum("1")).isEqualTo(1);
+        assertThat(Calculator.splitAndSum("1,2")).isEqualTo(3);
+        assertThat(Calculator.splitAndSum("1,2:3")).isEqualTo(6);
+        assertThat(Calculator.splitAndSum("//;\n1,2:3;4")).isEqualTo(10);
+    }
 }
