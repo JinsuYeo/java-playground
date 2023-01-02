0fc54eb3b6d8659910ebdc83c5f82026caa70f1a
jsyeo97 <jsyeo97@naver.com>
2023-01-02 16:41:18 +0900

    feat: 전체 입력 기능 구현 및 예외 처리 구현

---
+++ src/main/java/racingCar/GameController.java
@@ -0,0 +1,48 @@
+package racingCar;
+
+import jdk.internal.net.http.common.Pair;
+
+import java.util.List;
+
+public class GameController {
+
+    private static List<String> receiveCarNames() {
+        List<String> carNames;
+        try {
+            carNames = InputView.inputCarNames();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+        return carNames;
+    }
+
+    private static int receiveNumberOfAttempts() {
+        int attempt;
+        try {
+            attempt = InputView.inputNumberOfAttempts();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return 0;
+        }
+        return attempt;
+    }
+
+    private static List<String> receiveValidCarNames() {
+        List<String> carNames = null;
+        while((carNames = receiveCarNames()) == null);
+        return carNames;
+    }
+
+    private static int receiveValidNumberOfAttempts() {
+        int attempt = 0;
+        while ((attempt = receiveNumberOfAttempts()) == 0);
+        return attempt;
+    }
+
+    public static void main(String[] args) {
+        List<String> carNames = receiveValidCarNames();
+        int attempt = receiveValidNumberOfAttempts();
+        InputView.closeScanner();
+    }
+}
--- src/main/java/racingCar/InputView.java
+++ src/main/java/racingCar/InputView.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 
 public class InputView {
     private static String DELIMETER = ",";
+    private static Scanner scanner = new Scanner(System.in);
 
-    public static List<String> inputCarNames() {
-        Scanner scanner = new Scanner(System.in);
+    public static List<String> inputCarNames() throws IllegalArgumentException {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)");
         String inputString = scanner.nextLine();
-        scanner.close();
         return createCarNames(inputString);
     }
 
@@ -20,7 +20,7 @@ public class InputView {
         return carNames;
     }
 
-    private static void checkCarNames(List<String> carNames) throws IllegalArgumentException {
+    public static void checkCarNames(List<String> carNames) throws IllegalArgumentException {
         if(carNames.stream().anyMatch(name -> !isValidLength(name))) {
             throw new IllegalArgumentException("자동차 이름은 5자 이내여야 합니다.");
         }
@@ -34,4 +34,19 @@ public class InputView {
         return carName.length() <= 5;
     }
 
+    public static int inputNumberOfAttempts() throws IllegalArgumentException {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int attempt = 0;
+        try {
+            attempt = scanner.nextInt();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("숫자를 입력해 주세요.");
+        }
+
+        return attempt;
+    }
+
+    public static void closeScanner() {
+        scanner.close();
+    }
 }
