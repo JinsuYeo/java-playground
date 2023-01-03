a62be5e100dfa968d21ff0ad9395aa95fd176659
jsyeo97 <jsyeo97@naver.com>
2023-01-03 10:53:31 +0900

    feat: 랜덤으로 0~9까지의 수 생성 기능 구현

--- src/main/java/racingCar/CarGame.java
+++ src/main/java/racingCar/CarGame.java
@@ -8,12 +8,21 @@ import java.util.stream.Collectors;
 
 public class CarGame {
     List<Car> cars;
+    Random random;
 
     public CarGame(List<String> carNames) {
         cars = new ArrayList<>();
         for(String carName : carNames) {
             cars.add(new Car(carName));
         }
+        this.random = new Random();
+        random.setSeed(System.currentTimeMillis());
+    }
+
+    public void moveCar() {
+        cars.stream().forEach(car -> {
+            car.move(random.nextInt(9));
+        });
     }
 
     public void moveCar(int testNum) {
