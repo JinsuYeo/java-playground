496c957df28cd18e1873e50cb759889fd8598d7e
jsyeo97 <jsyeo97@naver.com>
2023-01-03 10:52:11 +0900

    feat: 차 전진 및 전진 결과('-'로 움직인 횟수를 표현) 생성 기능 구현

--- src/main/java/racingCar/CarGame.java
+++ src/main/java/racingCar/CarGame.java
@@ -1,2 +1,36 @@
-package racingCar;public class CarGame {
+package racingCar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class CarGame {
+    List<Car> cars;
+
+    public CarGame(List<String> carNames) {
+        cars = new ArrayList<>();
+        for(String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+    }
+
+    public void moveCar(int testNum) {
+        cars.stream().forEach(car -> {
+            car.move(testNum);
+        });
+    }
+
+    public List<String> getMovingResult() {
+        List<String> movingResult = new ArrayList<String>();
+        cars.stream().forEach(car -> {
+            String moveString = "-";
+            for(int i = 0; i < car.countMoving(); i++) {
+                moveString += "-";
+            }
+            movingResult.add(moveString);
+        });
+        return movingResult;
+    }
 }
